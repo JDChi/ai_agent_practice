808887aac9700304d1331c30bcc5e9d44157e28e
jdchi <chijiaduo@hotmail.com>
2024-06-09 21:13:24 +0800

    feat: using dall model to generate image

---
+++ dall.py
@@ -0,0 +1,23 @@
+import os
+import openai
+
+open_ai_host = os.environ.get("OPEN_AI_HOST")
+open_ai_api_key = os.environ.get("OPEN_AI_API_KEY")
+
+client = openai.OpenAI(
+    base_url=open_ai_host,
+    api_key=open_ai_api_key
+)
+
+response = client.images.generate(
+    model="dall-e-3",
+    prompt="小猫在月球的海报",
+    size="1024x1024",
+    quality="standard",
+    n=1
+)
+
+image_url = response.data[0].url
+
+# https://oaidalleapiprodscus.blob.core.windows.net/private/org-gc5o1ctsE7wMxsw4v2iVZH9q/addsad/img-I4LqpHlKYiIMWQ3VZSzNYq8f.png?st=2024-06-09T12%3A12%3A17Z&se=2024-06-09T14%3A12%3A17Z&sp=r&sv=2023-11-03&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-06-08T17%3A23%3A53Z&ske=2024-06-09T17%3A23%3A53Z&sks=b&skv=2023-11-03&sig=aIi/NTP0SfRD7gHACYAuyF7TGE7rJVacSgp7rUDcHQM%3D
+print(image_url)
