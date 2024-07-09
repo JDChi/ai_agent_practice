9db9950b9335c39b6e9ec5802180530566e4e967
jdchi <chijiaduo@52tt.com>
2024-07-09 09:39:21 +0800

    feat: split code

--- langchain/rag/spiltter.py
+++ langchain/rag/spiltter.py
@@ -1,14 +1,37 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-with open("../../assets/泡沫.txt") as f:
-    content = f.read()
+# with open("../../assets/泡沫.txt") as f:
+#     content = f.read()
+#
+# text_splitter = RecursiveCharacterTextSplitter(
+#     chunk_size=50,  # 切分的文本块大小
+#     chunk_overlap=20,  # 切分的文本块重叠大小
+#     length_function=len,  # 长度函数
+#     add_start_index=True,  # 是否添加开始索引
+# )
+#
+# text = text_splitter.create_documents([content])
+# print(text)
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=50,  # 切分的文本块大小
-    chunk_overlap=20,  # 切分的文本块重叠大小
-    length_function=len,  # 长度函数
-    add_start_index=True,  # 是否添加开始索引
+# split code
+
+from langchain.text_splitter import Language
+
+GO_CODE = """
+func IndexOf[T comparable](collection []T, element T) int {
+	for i, item := range collection {
+		if item == element {
+			return i
+		}
+	}
+
+	return -1
+}
+"""
+
+go_splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.GO, chunk_size=50, chunk_overlap=0
 )
 
-text = text_splitter.create_documents([content])
-print(text)
+go_docs = go_splitter.create_documents([GO_CODE])
+print(go_docs)
